44245ad8b388e22a76097c9d1d6350e408287ee4
Audrielle-KM <audrielle.myesha@gmail.com>
2026-03-20 14:24:43 +0000

    TA

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World from main and Q5 branches!");
+		System.out.println("Hello World from Q8!");
 	} // END main
 }
